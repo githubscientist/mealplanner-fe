4aea6538f1c0f094324bbb6f8fc003daf13cba3c
githubscientist <sathish.datascientist@gmail.com>
2025-09-21 11:31:03 +0530

    register, login pages created

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div className="flex flex-col items-center justify-center p-4">
+            <p className="text-lg max-w-2xl text-center">
+                Mealplanner is a web application designed to help you plan your meals efficiently. Whether you're looking to organize your weekly meals or find new recipes, Mealplanner has got you covered!
+            </p>
+        </div>
+    )
+}
+
+export default About;
